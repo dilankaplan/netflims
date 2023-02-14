df0473a30899ffa12b0c1d6fafcc8e2952bf94f6
Dilan Kaplan <45143995+dilankaplan@users.noreply.github.com>
2023-02-14 23:33:52 +0300

    api

--- src/app/[[...category]]/page.jsx
+++ src/app/[[...category]]/page.jsx
@@ -3,13 +3,24 @@ import HomeContainer from "@/containers/home";
 
 import Movies from "@/mocks/movies.json"
 
-function HomePage({params}) {
+const API_URL ='https://api.themoviedb.org/3'
+
+const getPopularMovies = async() => {
+  const res = await fetch(`${API_URL}/movie/top_rated?api_key=${process.env.API_KEY}&PAGE=1`);
+  const data = await res.json();
+  console.log(data);
+}
+ async function HomePage({params}) {
   let selectedCategory;
+
+  const popularMovies = await getPopularMovies(); 
 
   if(params.category?.length > 0){
    selectedCategory =true;
   }
-  return <HomeContainer selectedCategory={
+  return <HomeContainer 
+  popularMovies={popularMovies}
+     selectedCategory={
     {id: params.category?.[0] ?? '',
      movies: selectedCategory ? Movies.results.slice(0,7):[],
   }
--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -5,11 +5,13 @@ import Categories from "@/components/categories"
 import Genres from "@/mocks/genres.json"
 import { MoviesSection } from '@/components/movies-section'
 
-function HomeContainer({selectedCategory}) {
+ function HomeContainer({popularMovies = [],selectedCategory}) {
   return (
    <div>
     <FeaturedMovie movie={Movies.results[0]} />
+
     <Categories categories={Genres.genres.slice(0,5)}/> 
+
     {selectedCategory.movies.length > 0 &&(
       <MoviesSection 
     title={Genres.genres.find((genres)=>
@@ -20,7 +22,7 @@ function HomeContainer({selectedCategory}) {
    
     <MoviesSection 
     title=" Popular Films" 
-    movies={Movies.results.slice(1,7)} />
+    movies={popularMovies.slice(1,7)} />
 
     <MoviesSection 
     title="Your Favorites" 
